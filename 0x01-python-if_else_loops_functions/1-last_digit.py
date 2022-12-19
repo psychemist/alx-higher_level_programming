413c46e38d5c97b0adc961f4dbe6fa3ee77dcad5
psych0wh1z <ikeagudike@gmail.com>
2022-12-19 02:48:18 +0100

    Fix pycodestyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,10 @@ else:
 
 if mod > 5:
     print("Last digit of {} is {} and is greater than 5"
-        .format(number, mod))
+          .format(number, mod))
 elif mod < 6 and mod != 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
-        .format(number, mod))
+          .format(number, mod))
 else:
     print("Last digit of {} is {} and is 0"
-        .format(number, mod))
+          .format(number, mod))
