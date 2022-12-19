51c2c244c247b01d7ba7163c590c472d30497dfb
psych0wh1z <ikeagudike@gmail.com>
2022-12-19 02:47:05 +0100

    Fix modulo logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    mod = (number * -1) % 10
+    mod = number % -10
 else:
     mod = number % 10
-str = "Last digit of"
 
 if mod > 5:
-    print("{} {} is {} and is greater than 5".format(str, number, mod))
+    print("Last digit of {} is {} and is greater than 5"
+        .format(number, mod))
 elif mod < 6 and mod != 0:
-    print("{} {} is {} and is less than 6 and not 0".format(str, number, mod))
+    print("Last digit of {} is {} and is less than 6 and not 0"
+        .format(number, mod))
 else:
-    print("{} {} is {} and is 0".format(str, number, mod))
+    print("Last digit of {} is {} and is 0"
+        .format(number, mod))
