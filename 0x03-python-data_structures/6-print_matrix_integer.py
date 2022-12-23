2d588ae2ad06bfecc26de78f28a66c88b62c24c5
psych0wh1z <ikeagudike@gmail.com>
2022-12-23 12:42:27 +0100

    Add Python one-liner

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for grid in matrix:
-        for num in grid:
-            print("{:d} ".format(num), end="")
-        print()
+        print(" ".join("{:d}".format(num) for num in grid))
