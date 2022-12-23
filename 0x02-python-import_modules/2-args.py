d148a835c7cbc4f7e1a6139b78a169d9cf74f288
psych0wh1z <ikeagudike@gmail.com>
2022-12-23 01:44:38 +0100

    Add import block

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-if len(argv) == 1:
-    print("0 arguments.")
-else:
-    if len(argv) == 2:
-        print("1 argument:")
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print("0 arguments.")
     else:
-        print(f"{len(argv)-1} arguments:")
-    for i in range(len(argv)):
-        if i == 0:
-            continue
-        print("{:d}: {:s}".format(i, str(argv[i])))
+        if len(argv) == 2:
+            print("1 argument:")
+        else:
+            print(f"{len(argv)-1} arguments:")
+        for i in range(len(argv)):
+            if i == 0:
+                continue
+            print("{:d}: {:s}".format(i, str(argv[i])))
