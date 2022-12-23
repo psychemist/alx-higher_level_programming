4c891112cdae902042afbc8e780d793d9108b5c0
psych0wh1z <ikeagudike@gmail.com>
2022-12-23 12:19:54 +0100

    Remove extra spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for grid in matrix:
         for num in grid:
-            print("{:d} ".format(num), end=" ")
+            print("{:d} ".format(num), end="")
         print()
