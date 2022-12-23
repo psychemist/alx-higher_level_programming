37a6a4337b81e0b4bd8ce02a9fc6fbbaf1d37f95
psych0wh1z <ikeagudike@gmail.com>
2022-12-23 11:07:49 +0100

    Fix code logic

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
     for num in reversed(my_list):
-        else:
-            print("{:d}".format(num))
+        print("{:d}".format(num))
