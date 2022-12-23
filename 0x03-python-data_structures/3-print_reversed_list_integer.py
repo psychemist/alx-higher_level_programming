6e317a927c8cf62a4f5dd5b7fa3db760c64a30d8
psych0wh1z <ikeagudike@gmail.com>
2022-12-23 11:02:25 +0100

    Change None return value

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for num in reversed(my_list):
-        if my_list != None:
+        if my_list is not None:
             print("{:d}".format(num))
