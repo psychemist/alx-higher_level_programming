9f405c522d666c7357a1b767804e3179e68b69da
psych0wh1z <ikeagudike@gmail.com>
2022-12-24 13:46:49 +0100

    Refactor assignment

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,5 +7,5 @@ def max_integer(my_list=[]):
         maxi = my_list[0]
         for num in my_list:
             if num > maxi:
-                maxi += num - maxi
+                maxi = num
     return maxi
