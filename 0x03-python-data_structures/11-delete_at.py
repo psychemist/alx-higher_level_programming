f9062f4eda1669eed13b4c3f34cfdcd5ec7ba43d
psych0wh1z <ikeagudike@gmail.com>
2022-12-24 15:11:47 +0100

    Add IndexError failsafe

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or my_list[idx] == None:
+    if idx < 0 or IndexError:
         return my_list
     else:
         del my_list[idx]
