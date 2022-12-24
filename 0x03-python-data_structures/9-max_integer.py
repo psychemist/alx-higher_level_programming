f62fc0ce12598786cf4361fb5ba68845414019f4
psych0wh1z <ikeagudike@gmail.com>
2022-12-24 13:41:33 +0100

    Fix max start value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        maxi = 0
+        maxi = my_list[0]
         for num in my_list:
             if num > maxi:
                 maxi += num - maxi
