85f39c97ea53e6c497e3aa0237427c370e722475
psych0wh1z <ikeagudike@gmail.com>
2022-12-24 15:06:59 +0100

    Refactor code logic

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx >= 0 and my_list[idx]:
+    if idx < 0 or my_list[idx] == None:
+        return my_list
+    else:
         del my_list[idx]
+
     return my_list
