25ab35d60f108d35728c94bdcf0eeb2aa15c1c4e
psych0wh1z <ikeagudike@gmail.com>
2022-12-27 02:26:00 +0100

    Add Python one-liner solution

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,3 +4,5 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         b_dictionary[key] = value*2
     return b_dictionary
+
+# return {key: a_dictionary[key]*2 for key in a_dictionary}
