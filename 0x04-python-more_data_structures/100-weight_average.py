907530191367ce795453a95a4051473066dad25f
psych0wh1z <ikeagudike@gmail.com>
2022-12-27 04:37:58 +0100

    Fix empty list return error

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
     score, weight = 0, 0
     for (scores, weights) in my_list:
         score += scores * weights
