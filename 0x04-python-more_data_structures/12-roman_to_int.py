d4606b8f6dd7af6ac97f642e0942b1c79fb5c2e9
psych0wh1z <ikeagudike@gmail.com>
2022-12-27 03:53:55 +0100

    Fix pycodestyle error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or roman_string is None:
         return 0
     arab_num = 0
     for i in range(len(roman_string)):
