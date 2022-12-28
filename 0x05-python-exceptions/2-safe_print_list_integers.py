a596e0a0362f3339dac28d0a7be41d2447d27926
psych0wh1z <ikeagudike@gmail.com>
2022-12-28 11:26:21 +0100

    Add new counter variable

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    idx = 0
+    real = 0
     for idx in range(x):
         try:
             print("{:d}".format(my_list[idx]), end="")
-            idx += 1
+            real += 1
         except (TypeError, ValueError):
-            idx -= 1
             continue
     print()
-    return idx
+    return real
