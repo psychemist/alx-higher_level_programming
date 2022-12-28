0c9bd6576496e8ba5b043dde1b8dea0cccc4e5e0
psych0wh1z <ikeagudike@gmail.com>
2022-12-28 10:32:39 +0100

    Remove exception class

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-        except(IndexError, TypeError):
+        except(IndexError):
             break
         i += 1
     print()
