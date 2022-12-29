9ef2e8f4035861d2a0b76ea53505e7b475a8b45b
psych0wh1z <ikeagudike@gmail.com>
2022-12-29 23:26:09 +0100

    Fix pycodestyle error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,6 +19,6 @@ class Square:
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
-                raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         elif size >= 0:
-                self.__size = size
+            self.__size = size
