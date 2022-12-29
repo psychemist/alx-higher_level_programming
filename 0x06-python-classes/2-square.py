cfd10499ad53158b61ced0e3ca4aa38117d8f5fb
psych0wh1z <ikeagudike@gmail.com>
2022-12-29 23:20:32 +0100

    Rework code logic

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,10 +16,9 @@ class Square:
         Args:
             size (int): Length of one side of square
         """
-        try:
-            if size >= 0:
-                self.__size = size
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-        except TypeError:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+                raise ValueError("size must be >= 0")
+        elif size >= 0:
+                self.__size = size
