c53c7f22bf2d3f84a7c094585869cbbb304ebebb
psych0wh1z <ikeagudike@gmail.com>
2022-12-29 23:37:17 +0100

    Add task3 file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 3-square
+Defines a square based on 2-square.py
+"""
+
+
+class Square:
+    """
+    Instantiates square object with public and private attributes
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance
+
+        Args: size (int): Length of sides of square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Returns current square area
+        """
+        return self.__size ** 2
