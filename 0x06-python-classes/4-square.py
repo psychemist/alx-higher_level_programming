d7d89b890664fcc9570b31d3097beec7ad2850f2
psych0wh1z <ikeagudike@gmail.com>
2022-12-30 00:29:49 +0100

    Add task4 file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module 4-square
+Defines a square based on 3-square.py
+"""
+
+
+class Square:
+    """
+    Instantiates square object with a public instance method,
+    a private instance attribute and a getter and setterproperties
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance
+
+        Args: size (int): Length of sides of square
+
+        """
+        self.size = size
+        """size (int): length of one side of square"""
+
+    @property
+    def size(self):
+        """int: read-only size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the area of a square
+
+        Returns:
+            int: Area of the square
+
+        """
+        return self.__size ** 2
