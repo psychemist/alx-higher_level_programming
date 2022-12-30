834f14faf61896d1bb954602064ca63c9d253a9e
psych0wh1z <ikeagudike@gmail.com>
2022-12-30 01:35:27 +0100

    Add task6 file

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Module 6-square
+Defines class Square with private size and public area
+Can access and update size
+Can print to stdout the square using #'s
+"""
+
+
+class Square:
+    """
+    Instantiates square object with public instance methods, a private
+    instance attribute, and getter and setter properties for accessing
+    and updating hidden values.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes a new square instance
+
+        Args:
+            size (int): Length of sides of square
+            position (tuple): Tuple of two positive integers
+
+        """
+        self.size = size  #: size (int): length of square
+        self.position = position
+        """position(int, int): tuple of two +ve int"""
+
+    @property
+    def size(self):
+        """
+        Attribute:
+            size (int): read-write property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """
+        Attribute:
+            position (tuple): read-write property
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Calculates the area of a square
+
+        Returns:
+            int: Area of the square
+
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square instance with the # character
+           or nothing if size is 0
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for rows in range(self.__size):
+                print("\n".join([" " * self.__position[0]]), end="")
+                print("".join(["#" * self.__size]))
