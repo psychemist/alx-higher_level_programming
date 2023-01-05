935e0d7a61a522a78d472129d9f2433be37ef44f
psych0wh1z <ikeagudike@gmail.com>
2023-01-05 06:39:15 +0100

    Add tests and remove all attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,12 +9,16 @@ class LockedClass:
     """
     Prevents user from dynamically creating new instance attributes
     except if the new instance attribute is called first_name
+
+    >>> a = LockedClass()
+    >>> a.first_name = 'Agu'
+    >>> a.first_name
+    'Agu'
+
+    >>> a.last_name = 'Obunadike'
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'LockedClass' object has no attribute 'last_name'
     """
     __slots__ = ['first_name']
 
-    def __init__(self, first_name="None"):
-        """
-        Instantiates new LockedClass object
-        """
-        if first_name is not None:
-            self.first_name = first_name
