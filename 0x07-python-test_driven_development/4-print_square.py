d6f94fd245fe4570fd716dd301bc28189af96a65
psych0wh1z <ikeagudike@gmail.com>
2023-01-12 13:44:43 +0100

    Fix zero print code

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,8 +18,9 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     elif type(size) is float and size < 0:
         raise TypeError("size must be an integer")
-    elif size == 0:
-        print()
+
+    if size == 0:
+        print("", end="")
     else:
         for x in range(size):
             for y in range(size):
