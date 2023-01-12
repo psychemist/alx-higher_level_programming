8f12032976dd292543e1e8902ce8931e119509c3
psych0wh1z <ikeagudike@gmail.com>
2023-01-12 05:01:42 +0100

    Add task2 files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module 2-matrix_divided
+Defines a function that 
+"""
+
+matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception with the message matrix must be a matrix (list of lists) of integers/floats
+
+Each row of the matrix must be of the same size, otherwise raise a TypeError exception with the message Each row of the matrix must have the same size
+
+
+All elements of the matrix should be divided by div, rounded to 2 decimal places
+
+Returns a new matrix
+
+def matrix_divided(matrix, div):
+    """
+    """
+    if type
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return
