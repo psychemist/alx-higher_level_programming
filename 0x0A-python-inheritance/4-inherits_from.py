9df7662cddb1691388ed4c0063c7093f061cfbf1
psych0wh1z <ikeagudike@gmail.com>
2023-01-13 13:33:05 +0100

    Add task4 files

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 4-inherits_from
+
+Defines a function that checks an instance’s inheritance
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if an object instance inherited (directly or indirectly)
+    from the specified class
+
+    Args:
+        obj (object): user defined object instance
+        a_class (class): Python in-built class object
+
+    Returns:
+        (bool): True if an object instance or False if otherwise
+    """
+    return issubclass(type(obj), a_class)
