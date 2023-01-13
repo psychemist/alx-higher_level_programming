1ad1d6ba4adeeebb3a94ab2539177b52807864c7
psych0wh1z <ikeagudike@gmail.com>
2023-01-13 13:37:29 +0100

    Fix return for same class values

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,4 +18,6 @@ def inherits_from(obj, a_class):
     Returns:
         (bool): True if an object instance or False if otherwise
     """
-    return issubclass(type(obj), a_class)
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
+    return False
