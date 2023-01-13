9c30c343d68f935142207b3caab1cfc2d4874272
psych0wh1z <ikeagudike@gmail.com>
2023-01-13 11:24:59 +0100

    Add docstrings

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """
+Module 0-lookup
+
+Defines a function that lists the fields and methods of an object
 """
 
 
 def lookup(obj):
     """
+    Lists the available data and method attributes of an object
+
+    Args:
+        obj (object): class or instance object 
+
+    Returns:
+        (list): list object containing attribute in module namespace
     """
     return obj.__dict__
