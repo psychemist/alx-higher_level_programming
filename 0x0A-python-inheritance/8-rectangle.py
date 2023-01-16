4ffcb61a66b4b9059b83f466263722f117820753
psych0wh1z <ikeagudike@gmail.com>
2023-01-16 18:11:01 +0100

    Add task8 file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""Module 8-rectangle contains a class based on a class in 7-base_geometry
 """
-"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
+    """Defines a Rectangle class that inherits from BaseGeometry
     """
     def __init__(self, width, height):
         """
-        """
+        Instantiates an instance object
 
+        Args:
+            width (int): length of shoter side of rectangle
+            height (int): length of longer side of rectangle
+        """
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
