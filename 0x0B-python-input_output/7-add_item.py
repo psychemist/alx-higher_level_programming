4921fa677de90777159ab10bf97519fbc150b5db
psych0wh1z <ikeagudike@gmail.com>
2023-01-17 20:30:03 +0100

    Add try/except code block

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Module 7-add_item
-
-Adds all arguments to a Python list, and saves them to a JSON file
+Module 7-add_item adds all main arguments
+to a list and saves them to a JSON file
 """
 import sys
 import json
@@ -10,8 +9,13 @@ save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 
-my_list = []
-with open("add_item.json", mode="r+", encoding="utf-8") as f:
-    args = sys.argv[1:]
-    my_list.append(args)
-    json.dump(my_list, f)
+args = sys.argv[1:]
+filename = "add_item.json"
+
+try:
+    all_args = load(filename)
+except FileNotFoundError:
+    all_args = []
+
+all_args.extend(args)
+save(all_args, filename)
