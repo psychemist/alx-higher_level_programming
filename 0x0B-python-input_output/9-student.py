70ebc7d49a8fc8855e1682817ba826a566a573a7
psych0wh1z <ikeagudike@gmail.com>
2023-01-17 22:05:06 +0100

    Add task9 files

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module 9-student contains a class that retrieves
+a JSON serialization of its attributes
+"""
+
+
+class Student:
+    """Defines a class with public instance attributes and a public method
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiates each instance object with three
+        public instance attributes and a public method
+
+        Args:
+            first_name (str): first name of student
+            last_name (str): last name of student
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns:
+            Dictionary representation of a Student instance object
+        """
+        return {name: attr for name, attr in self.__dict__.items()
+                if not name.startswith("__")}
