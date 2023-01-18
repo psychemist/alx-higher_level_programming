43bc3ce9c830344a2ee081e47bfad05581b5eb06
psych0wh1z <ikeagudike@gmail.com>
2023-01-18 23:17:13 +0100

    Add more unit tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,16 @@
-"""Unittest for Base
+"""Unittest for Rectangle
 """
 import unittest
 from models.rectangle import Rectangle
 
 
 class TestBaseClass(unittest.TestCase):
+    def test_rect_id(self):
+        self.assertEqual(Rectangle(2, 5, 0, 0, 10).id, 10)
+
+    def test_no_id(self):
+        self.assertEqual(Rectangle(5, 10).id, 1)
+
+    def test_neg_id(self):
+        self.assertEqual(Rectangle(1, 2, 0, 0, -1).id, -1)
+
