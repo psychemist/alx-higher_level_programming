9fbf3db8f53014c036d698d369a715d54f4106f7
psych0wh1z <ikeagudike@gmail.com>
2023-01-18 20:43:57 +0100

    Add documentation

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
+Module 100-append_afte inserts a line of text to a
+file after each line containing a specific string
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Appends"""
+    """Appends a new string after each search string in a text file"""
     with open(filename, mode="r+", encoding="utf-8") as f:
         new_text = ""
         for line in f:
