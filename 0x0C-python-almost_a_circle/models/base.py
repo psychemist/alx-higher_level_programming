9763629d8b02f77cf78a8280dcbb5d937f42ba95
psych0wh1z <ikeagudike@gmail.com>
2023-01-19 17:15:43 +0100

    Add class method to base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,26 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries
+
+            Args:
+                list_dictionaries (list): dict of instances inherited from base
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file
+
+            Args:
+                list_objs (list): list of instances that inherit from Base
+        """
+        filename = f"{cls.__name__}.json"
+        obj_list = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                obj_list.append(cls.to_dictionary(obj))
+        with open(filename, mode="w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(obj_list))
