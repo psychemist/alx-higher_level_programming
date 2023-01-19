b18b7915bcfc20f4fc7d5d2b622c6b32eea191d1
psych0wh1z <ikeagudike@gmail.com>
2023-01-19 03:40:07 +0100

    Add Square model

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module square inherits from Rectangle class defined in rectangle module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square class with private instance attributes
+    and public instance methods
+
+    Attributes:
+        __size (int): private instance attribute representing length
+        __x (int): private instance attribute representing x-coordinate
+        __y (int): private instance attribute representing y-coordinate
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiates a new Square instance object
+
+        Args:
+            size (int): length of each side of square
+            x (int): horizonal position of object instance along x-axis
+            y (int): vertical position of object instance along y-axis
+        """
+        super().__init__(size, size, x, y, id)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,22 @@
+"""Unittest for Square
+"""
+import unittest
+from models.square import Square
+
+
+class TestBaseClass(unittest.TestCase):
+    def test_square_id(self):
+        self.assertEqual(Square(6).id, 5)
+        self.assertEqual(Square(3, 5, 7, 11).id, 11)
+
+    def test_size(self):
+        self.assertEqual(Square(8).size, 8)
+
+    def test_coordinates(self):
+        self.assertEqual(Square(10, 3, 2).x, 3)
+        self.assertEqual(Square(10, 3, 2).y, 2)
+
+    def test_square_area(self):
+        self.assertEqual(Square(6).area, 36)
+        self.assertEqual(Square(7, ).area, 49)
+
