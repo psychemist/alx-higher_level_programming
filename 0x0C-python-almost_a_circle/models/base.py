1ac456542f01a6fdd1108062c59a94eac8a9dd9a
psych0wh1z <ikeagudike@gmail.com>
2023-01-19 16:09:17 +0100

    Fix json return value

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,6 +28,6 @@ class Base():
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
