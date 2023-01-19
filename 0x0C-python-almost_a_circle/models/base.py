43ac648d899f5b04c5d798ea416de6af15d0ae9d
psych0wh1z <ikeagudike@gmail.com>
2023-01-19 16:03:42 +0100

    Update base model with json functionality

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module base contains the base class of future classes
 """
+import json
 
 
 class Base():
@@ -13,8 +14,7 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Base class constructor
+        """Base class constructor
 
         Args:
             id (int): unique id of class instance object
@@ -24,3 +24,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
