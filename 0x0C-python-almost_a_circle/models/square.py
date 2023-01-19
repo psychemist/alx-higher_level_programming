b11d3061ec08aeb7a3d4768d99984eef63d5cd04
psych0wh1z <ikeagudike@gmail.com>
2023-01-19 15:53:34 +0100

    Update square class with new method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -54,3 +54,9 @@ class Square(Rectangle):
         else:
             for k, v in kwargs.items():
                 setattr(self, k, v)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square instance"""
+        dicto = {'id': self.id, 'size': self.size,
+                 'x': self.x, 'y': self.y}
+        return dicto
