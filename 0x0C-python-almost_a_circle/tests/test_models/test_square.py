c33e8ba207e95e0a801403dcf2b0b4fae9bf3a01
psych0wh1z <ikeagudike@gmail.com>
2023-01-19 21:17:32 +0100

    Fix test for area method

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -6,7 +6,7 @@ from models.square import Square
 
 class TestBaseClass(unittest.TestCase):
     def test_square_id(self):
-        self.assertEqual(Square(6).id, 5)
+        self.assertEqual(Square(6).id, 6)
         self.assertEqual(Square(3, 5, 7, 11).id, 11)
 
     def test_size(self):
@@ -17,6 +17,6 @@ class TestBaseClass(unittest.TestCase):
         self.assertEqual(Square(10, 3, 2).y, 2)
 
     def test_square_area(self):
-        self.assertEqual(Square(6).area, 36)
-        self.assertEqual(Square(7, ).area, 49)
+        self.assertEqual(Square(6).area(), 36)
+        self.assertEqual(Square(7, ).area(), 49)
 
