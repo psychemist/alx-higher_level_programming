17c8767ffd4bae0aaff11828fe97a464857ef6ef
psych0wh1z <ikeagudike@gmail.com>
2023-02-18 06:23:51 +0100

    Include guard for already existing table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table 'second_table' in the database
 -- in MySQL server and add multiple rows
 
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
