9d62a170f3c9b9d8f99034a2a85ee9759722d7ba
psych0wh1z <ikeagudike@gmail.com>
2023-02-18 10:58:50 +0100

    Fix column name output

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- displays the max temperature of each state (ordered by State name)
 
 SELECT state,
-       MAX(value)
+       MAX(value) 'max_temp'
   FROM temperatures
  GROUP BY state
  ORDER BY state ASC;
