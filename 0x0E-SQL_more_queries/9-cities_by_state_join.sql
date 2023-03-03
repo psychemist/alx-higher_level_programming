5979ab52828c9609cd89530689d9330569ac95a6
110nard0 <ikeagudike@gmail.com>
2023-03-02 16:07:46 -0800

    Fix indentation style error

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,6 +3,7 @@
 SELECT cities.id,
        cities.name,
        states.name
-  FROM cities INNER JOIN states
-    ON cities.state_id = states.id
+  FROM cities
+       INNER JOIN states
+       ON cities.state_id = states.id
  ORDER BY cities.id ASC;
