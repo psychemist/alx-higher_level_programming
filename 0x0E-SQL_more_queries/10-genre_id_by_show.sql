10f43db363998faae6d44c1bcd6f3f425fa2abc3
110nard0 <ikeagudike@gmail.com>
2023-03-02 16:45:33 -0800

    Modify join query

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,5 +6,4 @@ SELECT tv_shows.title,
   FROM tv_shows
        INNER JOIN tv_show_genres
        ON tv_shows.id = tv_show_genres.show_id
-          AND tv_show_genres.genre_id IS NOT NULL 
  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
