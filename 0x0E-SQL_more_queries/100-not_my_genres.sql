66653302b506cd30661daac5312da2a2d4294ecb
110nard0 <ikeagudike@gmail.com>
2023-03-03 20:28:50 -0800

    Fix spaces

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,10 +4,10 @@ SELECT name
   FROM tv_genres
  WHERE name NOT IN
        (SELECT name
-	  FROM tv_genres
-	  JOIN tv_show_genres
-	    ON tv_genres.id = tv_show_genres.genre_id
-	  JOIN tv_shows
-	    ON tv_shows.id = tv_show_genres.show_id
-	 WHERE title = "Dexter")
+          FROM tv_genres
+          JOIN tv_show_genres
+            ON tv_genres.id = tv_show_genres.genre_id
+          JOIN tv_shows
+            ON tv_shows.id = tv_show_genres.show_id
+         WHERE title = "Dexter")
  ORDER BY name ASC;
