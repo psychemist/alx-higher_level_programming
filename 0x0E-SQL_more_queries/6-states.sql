15e551ebd15a90216989e636d745a1f1281a1fc3
psych0wh1z <ikeagudike@gmail.com>
2023-03-04 05:05:30 +0100

    Fix style errors

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,6 +6,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
-        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
-	);
+        PRIMARY KEY (id),
+        id   INT          NOT NULL UNIQUE AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL
+);
