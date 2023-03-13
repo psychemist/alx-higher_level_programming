a8d0053acc82fa32c2165fa7eb9db5a8ba416e2f
psych0wh1z <ikeagudike@gmail.com>
2023-03-13 21:48:43 +0100

    Remove all variables

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-let str1 = 'C is fun';
-let str2 = 'Python is cool';
-let str3 = 'JavaScript is amazing';
-
-console.log(str1);
-console.log(str2);
-console.log(str3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
