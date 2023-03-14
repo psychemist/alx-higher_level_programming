a13063057ab80ae0bc04ddba873d0d84d950ad41
psych0wh1z <ikeagudike@gmail.com>
2023-03-14 02:57:20 +0100

    Add sorting compare function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
 
-const process = require('process');
-const argv = process.argv;
+const arr = process.argv.slice(2);
 
-if (argv.length > 3) {
-  let i = 2;
-  let j = 0;
-  const arr = [];
-
-  while (argv[i]) {
-    arr[j] = parseInt(argv[i]);
-    i++; j++;
-  }
-
-  const len = arr.length;
-  const sorted = arr.sort();
-  console.log(sorted[len - 2]);
-} else console.log(0);
+if (arr.length < 2) {
+  console.log('0');
+} else {
+  arr.sort((x, y) => x - y);
+  arr.pop();
+  console.log(arr.pop());
+}
