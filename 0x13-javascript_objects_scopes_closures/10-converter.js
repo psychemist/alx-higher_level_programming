94e852b4f36c703aca78482d9d3970fbaae53550
psych0wh1z <ikeagudike@gmail.com>
2023-03-15 23:27:40 +0100

    Add task10 files

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return Number();
+  return function convert (n) {
+    return n.toString(base);
+  }
 }
--- 0x13-javascript_objects_scopes_closures/10-main.js
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -6,7 +6,7 @@ let myConverter = converter(10);
 console.log(myConverter(2));
 console.log(myConverter(12));
 console.log(myConverter(89));
-
+console.log('\n');
 
 myConverter = converter(16);
 
