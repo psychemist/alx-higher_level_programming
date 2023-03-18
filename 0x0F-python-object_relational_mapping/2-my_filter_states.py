0320fc55e299b1af2c54ba7980280eabaf7ee62e
psych0wh1z <ikeagudike@gmail.com>
2023-03-19 00:58:02 +0100

    Include guarding clause

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,12 +9,15 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(
             host="localhost", port=3306,
             user=argv[1], passwd=argv[2], db=argv[3])
-    # activate cursor
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC"
-                .format(argv[4]))
+    # activate cursor and execute query
+    cursor = conn.cursor()
+    query = """SELECT * FROM states
+               WHERE name = '{:s}'
+               ORDER BY id ASC""".format(argv[4])
+    cursor.execute(query)
     # print rows
-    for row in cur.fetchall():
-        print(row)
-    cur.close()
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
     conn.close()
