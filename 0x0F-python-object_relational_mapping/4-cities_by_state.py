2d3b0d11607104379871e3cb85896cb887fbaae7
psych0wh1z <ikeagudike@gmail.com>
2023-03-19 01:39:45 +0100

    Add task4 files

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+4-cities_by_state module lists all cities
+from the database hbtn_0e_0_usa
+while preventing SQL injections
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    # connect to database
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    # activate cursor and execute query
+    cursor = conn.cursor()
+    query = """SELECT cities.id, cities.name, states.name
+               FROM cities
+               JOIN states
+               ON cities.state_id = states.id
+               ORDER BY cities.id ASC;
+            """
+    cursor.execute(query)
+    # print rows
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    conn.close()
