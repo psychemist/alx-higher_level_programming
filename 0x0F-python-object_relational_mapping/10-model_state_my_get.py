78dc93e12ff601c123f8b01b3eb205a07995b105
psych0wh1z <ikeagudike@gmail.com>
2023-03-19 18:47:48 +0100

    Add task10 file

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+lists State objects that contain the letter a from the database hbtn_0e_6_usa
+'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # establish connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # construct orm query
+    session = Session(engine)
+    valid = session.query(State).filter(State.name == argv[4]).all()
+    if not valid:
+       print("Not found")
+    else:
+       print("{}".format(valid[0].id))
+
+    session.close()
