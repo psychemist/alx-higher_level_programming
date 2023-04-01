f1f7de87a53f47d59e656a9a09e6b869faee24b9
psych0wh1z <ikeagudike@gmail.com>
2023-04-01 06:15:50 +0100

    Fix string output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-'''fetches status of a named URL
 '''
+fetches status of a named URL and displays response body
+'''
+
 import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen(
             'https://alx-intranet.hbtn.io/status') as response:
         status = response.read()
-        print("Body of response:")
+        print("Body response:")
         print("\t- type: {}".format(type(status)))
         print("\t- content: {}".format(status))
         print("\t- utf8 content: {}".format(status.decode('utf-8')))
