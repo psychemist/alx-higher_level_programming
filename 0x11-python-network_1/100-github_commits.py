162a7438186080c9f6e5b406337de5b94d12e175
psych0wh1z <ikeagudike@gmail.com>
2023-04-05 17:50:45 +0100

    Add task10 file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+takes 2 arguments and prints commits from a
+GitHub repository by shaID and author name
+'''
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    r = requests.get("https://api.github.com/repos/{}/{}/commits".
+                     format(argv[2], argv[1]))
+    for commit in r.json():
+        print(f"{commit.get('sha')}: {commit.get('commit')['author']['name']}")
