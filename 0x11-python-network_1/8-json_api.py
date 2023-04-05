f6833348f29e8c5e7220e7b475c4892f6b9c2593
psych0wh1z <ikeagudike@gmail.com>
2023-04-05 16:34:45 +0100

    Change exception type

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
             print("[{}] {}".format(res.get('id'), res.get('name')))
         else:
             print("No result")
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.JSONDecodeError as e:
         print("Not a valid JSON")
