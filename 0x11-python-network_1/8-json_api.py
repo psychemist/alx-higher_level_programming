79ff055df1bb176d12cfc2e00ae41caa60366e14
psych0wh1z <ikeagudike@gmail.com>
2023-04-05 16:31:03 +0100

    Add task8 file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+sends a POST request to http://0.0.0.0:5000/search_user
+using an accepted argument as a parameter
+'''
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        num = argv[1]
+    else:
+        num = ""
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': num})
+
+    try:
+        res = r.json()
+        if res:
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+        else:
+            print("No result")
+    except requests.exceptions.RequestException as e:
+        print("Not a valid JSON")
