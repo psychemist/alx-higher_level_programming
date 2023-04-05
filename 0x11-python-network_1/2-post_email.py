0343317c4deca95ddb13343f2f4c7efe55e63aa7
psych0wh1z <ikeagudike@gmail.com>
2023-04-05 07:29:01 +0100

    Add task2 file

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,10 +3,17 @@
 takes in a URL and an email, sends a POST request to the passed URL
 with the email as a parameter, and displays the body of the response
 '''
+
 from sys import argv
 from urllib import parse, request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(argv[1]) as response:
-        status = response.read()
-        print(response.__dict__.get('headers').get('X-Request-Id'))
+    url = argv[1]
+    values = {'email' : argv[2]}
+
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
