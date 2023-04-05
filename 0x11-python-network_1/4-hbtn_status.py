70d10cb6efa791b881587ce5461d8515486c7bf1
psych0wh1z <ikeagudike@gmail.com>
2023-04-05 08:24:04 +0100

    Add task4 file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+fetches status of a named URL and displays response body
+'''
+
+import requests
+
+if __name__ == "__main__":
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
