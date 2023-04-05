c4a3389832f5692aa5d8d656543d2314398a8ee6
psych0wh1z <ikeagudike@gmail.com>
2023-04-05 08:09:20 +0100

    Add task3 file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+'''
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print("Error code: ", e.code)
