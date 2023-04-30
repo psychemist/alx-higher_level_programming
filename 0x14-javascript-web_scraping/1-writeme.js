98e36a63325b09607e597192bee87f7451c19a10
psych0wh1z <ikeagudike@gmail.com>
2023-04-30 15:22:57 +0100

    Add task1 file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = require('process').argv;
+
+fs.writeFile(argv[2], argv[3], 'utf8', (err) => {
+  if (err) console.log(err);
+  console.log('File saved successfully');
+});
