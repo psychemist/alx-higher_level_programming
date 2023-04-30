f57763ee7b46115be820fa46111b5a26d5dab650
psych0wh1z <ikeagudike@gmail.com>
2023-04-30 15:14:23 +0100

    Add task0 file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = require('process').argv;
+
+fs.readFile(argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+})
