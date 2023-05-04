d070a8e3be00163f958f4e08a443003870ae25bc
psych0wh1z <ikeagudike@gmail.com>
2023-05-04 01:59:16 +0100

    Add task12 files

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+$('document').ready(function () {
+  $('input#btn_translate').on('click', function () {
+    const lang = $('input#language_code').val();
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+
+    $.get(url, function (data) {
+      $('div#hello').text(data.hello);
+    })
+  })
+});
